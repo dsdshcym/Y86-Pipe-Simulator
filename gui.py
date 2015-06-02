5a33861098b294b77d31cbde220afd54d5beb346
dsdshcym <dsdshcym@gmail.com>
2015-06-02 14:10:40 +0800

    Add more menubar init functions

--- gui.py
+++ gui.py
@@ -31,14 +31,53 @@ class MainWindow(QMainWindow):
         open_file.setStatusTip('Load a .yo file')
         open_file.triggered.connect(self.show_file_dialog)
 
+        run_file = QAction('Run', self)
+        run_file.setShortcut('Ctrl+R')
+        run_file.setStatusTip('Run a .yo file')
+        run_file.triggered.connect(self.run)
+
+        step = QAction('Step', self)
+        step.setShortcut('Ctrl+S')
+        step.setStatusTip('Run for one step')
+        step.triggered.connect(self.step)
+
+        back = QAction('Back', self)
+        back.setShortcut('Ctrl+B')
+        back.setStatusTip('Go back one step')
+        back.triggered.connect(self.back)
+
+        reset = QAction('Reset', self)
+        reset.setShortcut('Alt+R')
+        reset.setStatusTip('Reset a running processor')
+        reset.triggered.connect(self.reset)
+
         menubar = self.menuBar()
         file_menu = menubar.addMenu('&File')
         file_menu.addAction(open_file)
+        processor_menu = menubar.addMenu('&Processor')
+        processor_menu.addAction(run_file)
+        processor_menu.addAction(step)
+        processor_menu.addAction(back)
+        processor_menu.addAction(reset)
 
     def show_file_dialog(self):
         fname = QFileDialog.getOpenFileName(self, 'Load file', '~')
 
-        print fname
+    def run(self):
+        print('run')
+        pass
+
+    def step(self):
+        print('step')
+        pass
+
+    def back(self):
+        print('back')
+        pass
+
+    def reset(self):
+        print('reset')
+        pass
 
 # if __name__ == '__main__':
 app = QApplication(sys.argv)
